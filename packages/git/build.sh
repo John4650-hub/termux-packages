16710c0c5a22da8bfa03b009833edb05016f848f
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2023-01-05 09:33:33 +0300

    Update build.sh

--- packages/git/build.sh
+++ packages/git/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast, scalable, distributed revision control system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.39.0
+TERMUX_PKG_API_LEVEL=23
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/pub/software/scm/git/git-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ba199b13fb5a99ca3dec917b0bd736bc0eb5a9df87737d435eddfdf10d69265b
 TERMUX_PKG_DEPENDS="libcurl, libiconv, less, openssl, pcre2, zlib"
