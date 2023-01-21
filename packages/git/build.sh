f40ee6c6e4a44593ba375a35d4aa19fcf319a653
john4650-hub <johndelvin51@gmail.com>
2023-01-21 20:47:50 +0300

    build git

--- packages/git/build.sh
+++ packages/git/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Fast, scalable, distributed revision control system"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.39.1
+TERMUX_PKG_API_LEVEL=24
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/pub/software/scm/git/git-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=40a38a0847b30c371b35873b3afcf123885dd41ea3ecbbf510efa97f3ce5c161
 TERMUX_PKG_DEPENDS="libcurl, libiconv, less, openssl, pcre2, zlib"
