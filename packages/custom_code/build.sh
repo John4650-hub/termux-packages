47e184c3eb84084ddaf4297ea8c0322b231e78fc
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2024-12-02 10:12:35 +0300

    Update file via API

--- packages/custom_code/build.sh
+++ packages/custom_code/build.sh
@@ -1,12 +1,11 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/John4650-hub/my-Termux-packs
-TERMUX_PKG_DESCRIPTION="my first package"
+TERMUX_PKG_DESCRIPTION="Just something i use to create binaries for my codes"
 TERMUX_PKG_API_LEVEL=23
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@john4650-hub"
 TERMUX_PKG_VERSION=1:0.3
 TERMUX_PKG_SRCURL=https://github.com/John4650-hub/my-Termux-packs/archive/refs/tags/44.0.0.tar.gz
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
-#513fd4cb803d8b43fa488aab9d4a6d9d6221c5ead334cf62f3782bf736ab75a5
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
