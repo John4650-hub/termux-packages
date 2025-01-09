78e81979b20ee201095606db68ead89f4474cbbf
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-01-09 23:08:07 +0300

    commited changes

--- packages/custom_code/build.sh
+++ packages/custom_code/build.sh
@@ -1,4 +1,4 @@
-apt-get install astyle
+sudo apt-get install astyle
 TERMUX_PKG_HOMEPAGE=https://github.com/John4650-hub/my-Termux-packs
 TERMUX_PKG_DESCRIPTION="Justy something i use to create binaries for my codes."
 TERMUX_PKG_API_LEVEL=23
