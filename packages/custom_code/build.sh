65dd624a30b0edf808e242f71bf3014de1d914b9
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-01-13 03:42:27 +0300

    commited changes

--- packages/custom_code/build.sh
+++ packages/custom_code/build.sh
@@ -1,12 +1,10 @@
-latest_tag=$(curl -s https://api.github.com/repos/John4650-hub/my-Termux-packs/releases/latest | jq -r .tag_name)
-#latest_tag="154.0.0"
 TERMUX_PKG_HOMEPAGE=https://github.com/John4650-hub/my-Termux-packs
 TERMUX_PKG_DESCRIPTION="Justy something i use to create binaries for my codes.yes"
 TERMUX_PKG_API_LEVEL=23
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@john4650-hub"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SRCURL=https://github.com/John4650-hub/my-Termux-packs/archive/refs/tags/${latest_tag}.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/John4650-hub/my-Termux-packs/archive/refs/tags/$(curl -s https://api.github.com/repos/John4650-hub/my-Termux-packs/releases/latest | jq -r .tag_name).tar.gz
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_BUILD_DEPENDS="libftxui"
