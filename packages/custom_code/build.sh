f04e75dd0b202d51f02853912a74f6f47e7c33d9
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-01-13 14:57:53 +0300

    commited changes

--- packages/custom_code/build.sh
+++ packages/custom_code/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/John4650-hub/my-Termux-packs
 TERMUX_PKG_DESCRIPTION="Justy something i use to create binaries for my codes.yes"
-TERMUX_PKG_API_LEVEL=23
+#TERMUX_PKG_API_LEVEL=23
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@john4650-hub"
 TERMUX_PKG_VERSION=1.0.0
