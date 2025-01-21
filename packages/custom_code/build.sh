ebd5f2df5110ee6549612ae2def6c955ed158d55
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-01-21 15:45:13 +0300

    commited changes

--- packages/custom_code/build.sh
+++ packages/custom_code/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SRCURL=https://github.com/John4650-hub/my-Termux-packs/archive/refs/tags/$(curl -s https://api.github.com/repos/John4650-hub/my-Termux-packs/releases/latest | jq -r .tag_name).tar.gz
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_ESSENTIAL=true 
-TERMUX_PKG_DEPENDS="libftxui"
-TERMUX_PKG_BUILD_DEPENDS="libftxui"
+TERMUX_PKG_DEPENDS="libavcodec, libavutil, libavformat, libswresample" #"libftxui"
+TERMUX_PKG_BUILD_DEPENDS="libavcodec, libavutil, libavformat, libswresample" #"libftxui"TERMUX_PKG_BUILD_DEPENDS="libftxui"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
