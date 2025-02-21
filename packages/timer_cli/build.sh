eafc5bd570f362b5b11b8877909f1f2519dd3957
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-02-21 12:15:48 +0300

    Created new package toboe

---
+++ packages/timer_cli/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/John4650-hub/my-Termux-packs
+TERMUX_PKG_DESCRIPTION="A timer for my personal use"
+TERMUX_PKG_API_LEVEL=23
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@john4650-hub"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/John4650-hub/my-Termux-packs/archive/refs/tags/$(curl -s https://api.github.com/repos/John4650-hub/my-Termux-packs/releases/latest | jq -r .tag_name).tar.gz
+TERMUX_PKG_SHA256=SKIP_CHECKSUM
+TERMUX_PKG_ESSENTIAL=true
+TERMUX_PKG_DEPENDS="ffmpeg, libftxui"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+termux_dowload "https://maven.google.com/com/google/oboe/oboe/1.9.3/oboe-1.9.3.aar"
+ls
+pwd
+echo "$TERMUX__PREFIX/lib"
+ls -a "$TERMUX__PREFIX/lib"
