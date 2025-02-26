2e6ece74964d47da7822301934850feb4e081553
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-02-26 05:05:30 +0300

    remove some files which are partbof the source code from toboe package and timer_cli

--- packages/timer_cli/build.sh
+++ packages/timer_cli/build.sh
@@ -20,7 +20,7 @@ curl -L "https://github.com/John4650-hub/my-Termux-packs/archive/refs/tags/741.0
 unzip ffmpeg.zip -d dir
 cp dir/my-Termux-packs-741.0.0/src/lib/* "$TERMUX_PREFIX/lib/" -r
 cd dir/my-Termux-packs-741.0.0/src/include/
-rm player.hpp counter.hpp
+rm *.hpp
 cd -
 cp -r dir/my-Termux-packs-741.0.0/src/include/* "$TERMUX_PREFIX/include/"
 rm -rf dir/ ffmpeg.zip foo/ oboe.aar
