7d49bd4026d30513040e2a393eb2dec634fcae70
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-03-19 04:12:41 +0300

    JRE17 for android 6.0

--- packages/jre/build.sh
+++ packages/jre/build.sh
@@ -1,1 +1,17 @@
-foo
+TERMUX_PKG_HOMEPAGE=https://github.com/John4650-hub/my-Termux-packs
+TERMUX_PKG_DESCRIPTION="JRE for android 6.0 java 17"
+#TERMUX_PKG_API_LEVEL=23
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@john4650-hub"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/John4650-hub/my-Termux-packs/archive/refs/tags/$(curl -s https://api.github.com/repos/John4650-hub/my-Termux-packs/releases/latest | jq -r .tag_name).tar.gz
+TERMUX_PKG_SHA256=SKIP_CHECKSUM
+TERMUX_PKG_ESSENTIAL=true
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+curl -L "https://github.com/PojavLauncherTeam/android-openjdk-build-multiarch/releases/download/jre17-ec28559/jre17-arm-20210914-release.tar.xz" -o jre17.xz
+tar -xf jre17.xz
+# get lib
+cp lib/server/libjvm.so "$TERMUX_PREFIX/lib"
+
