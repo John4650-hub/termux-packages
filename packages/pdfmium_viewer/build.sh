b5ea9aa734c05c08d18526845cd9d3269ce8806b
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-10 09:00:26 +0300

    commited changes

--- packages/pdfmium_viewer/build.sh
+++ packages/pdfmium_viewer/build.sh
@@ -11,12 +11,21 @@ TERMUX_PKG_DEPENDS="libpng"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 
+TERMUX_INCLUDE_DIR="$TERMUX_PREFIX/include"
+TERMUX_LIB_DIR="$TERMUX_PREFIX/lib"
+#download pdfium-no-v8
 curl -L "https://github.com/bblanchon/pdfium-binaries/releases/latest/download/pdfium-android-arm.tgz" -o pdfium-android-arm.tgz
 mkdir foo/ && cd foo
 tar -xvzf ../pdfium-android-arm.tgz
 cd ..
 # get lib
-cp foo/lib/libpdfium.so "$TERMUX_PREFIX/lib"
+cp foo/lib/libpdfium.so "$TERMUX_LIB_DIR"
 # get headers
 # ok
-cp -r foo/include/* "$TERMUX_PREFIX/include"
+cp -r foo/include/* "$TERMUX_INCLUDES_DIR"
+# get opencv
+
+curl -LO "https://github.com/nihui/opencv-mobile/releases/download/v32/opencv-mobile-2.4.13.7-android.zip"
+unzip "opencv-mobile-2.4.13.7-android.zip" -d ./
+cp -r opencv-mobile-2.4.13.7-android/sdk/native/jni/include/* "$TERMUX_INCLUDES_DIR"
+cp -r opencv-mobile-2.4.13.7-android/sdk/native/libs/armeabi-v7a/* "$TERMUX_LIB_DIR"
