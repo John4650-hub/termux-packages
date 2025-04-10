2cda3adc3fb2edaf08d3aea0bd34a7e3057eb59e
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-10 19:55:18 +0300

    installed more packages from grimler.se

--- packages/pdfmium_viewer/build.sh
+++ packages/pdfmium_viewer/build.sh
@@ -30,7 +30,18 @@ unzip "opencv-mobile-2.4.13.7-android.zip" -d ./
 cp -r opencv-mobile-2.4.13.7-android/sdk/native/jni/include/* "$TERMUX_INCLUDE_DIR"
 cp -r opencv-mobile-2.4.13.7-android/sdk/native/libs/armeabi-v7a/* "$TERMUX_LIB_DIR"
 
-#get libomp.a
-curl -L "https://grimler.se/termux-main/pool/main/c/clang/clang_20.1.2_arm.deb" -o llvm.deb
+#get libomp.a //depended on by opencv
+curl -L "https://grimler.se/termux-ma,in/pool/main/c/clang/clang_20.1.2_arm.deb" -o llvm.deb
 dpkg -x llvm.deb llvm
 cp llvm/data/data/com.termux/files/usr/lib/libomp.a "$TERMUX_LIB_DIR"
+
+#get tesseract
+curl -L "https://grimler.se/termux-main/pool/main/t/tesseract/tesseract_5.5.0-2_arm.deb" -o tesseract.deb
+dpkg -x tesseract.deb tesseract
+cp -r tesseract/data/data/com.termux/files/usr/* "$TERMUX_PREFIX"
+
+#get leptonica
+curl -L "https://grimler.se/termux-main/pool/main/l/leptonica-static/leptonica-static_1.85.0_arm.deb" -o leptonica.deb
+dpkg -x leptonica.deb leptonica
+cp -r leptonica/data/data/com.termux/files/usr/* "$TERMUX_PREFIX"
+
