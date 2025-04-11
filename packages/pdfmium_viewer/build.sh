e861bf3be691cc8ad36a142b511b2f6a563be3b6
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-11 21:36:53 +0300

    forgot tp set api level after test

--- packages/pdfmium_viewer/build.sh
+++ packages/pdfmium_viewer/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A pdfViewer using pdfmium"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@john4650-hub"
 TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_API_LEVEL=23
 TERMUX_PKG_SRCURL=https://github.com/John4650-hub/my-Termux-packs/archive/refs/tags/$(curl -s https://api.github.com/repos/John4650-hub/my-Termux-packs/releases/latest | jq -r .tag_name).tar.gz
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_ESSENTIAL=true
