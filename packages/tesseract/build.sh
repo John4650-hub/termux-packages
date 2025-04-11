86b3963f7a31125ac08f6fb9a8d2726b6bf63a12
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-11 08:16:25 +0300

    Updated things

--- packages/tesseract/build.sh
+++ packages/tesseract/build.sh
@@ -4,6 +4,7 @@ TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="5.5.0"
 TERMUX_PKG_REVISION=2
+TERMUX_PKG_API_LEVEL=23
 TERMUX_PKG_SRCURL=https://github.com/tesseract-ocr/tesseract/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f2fb34ca035b6d087a42875a35a7a5c4155fa9979c6132365b1e5a28ebc3fc11
 TERMUX_PKG_AUTO_UPDATE=true
@@ -60,4 +61,30 @@ termux_step_post_make_install() {
 		$TERMUX_PKG_CACHEDIR/tessdata/eng.traineddata \
 		daa0c97d651c19fba3b25e81317cd697e9908c8208090c94c3905381c23fc047
 	cp $TERMUX_PKG_CACHEDIR/tessdata/eng.traineddata .
+}
+doo(){
+curl -LO "https://grimler.se/termux-main/pool/main/f/fontconfig/fontconfig_2.16.1_arm.deb" -LO "https://grimler.se/termux-main/pool/main/g/glib/glib_2.84.1_arm.deb" -LO "https://grimler.se/termux-main/pool/main/h/harfbuzz/harfbuzz_11.0.1_arm.deb" -LO "https://grimler.se/termux-main/pool/main/l/leptonica/leptonica_1.85.0_arm.deb" -LO "https://grimler.se/termux-main/pool/main/liba/libandroid-glob/libandroid-glob_0.6-2_arm.deb" -LO "https://grimler.se/termux-main/pool/main/liba/libandroid-posix-semaphore/libandroid-posix-semaphore_0.1-3_arm.deb" -LO "https://grimler.se/termux-main/pool/main/liba/libarchive/libarchive_3.7.9_arm.deb" -LO "https://grimler.se/termux-main/pool/main/libc/libc++/libc++_27c_arm.deb" -LO "https://grimler.se/termux-main/pool/main/libc/libcairo/libcairo_1.18.4-1_arm.deb" -LO "https://grimler.se/termux-main/pool/main/libc/libcurl/libcurl_8.13.0_arm.deb" -LO "https://grimler.se/termux-main/pool/main/libi/libicu/libicu_77.1-1_arm.deb" -LO "https://grimler.se/termux-main/pool/main/p/pango/pango_1.56.3-1_arm.deb" -LO "https://grimler.se/termux-main/pool/main/libc/libcpufeatures/libcpufeatures_0.9.0_arm.deb" -LO "https://grimler.se/termux-main/pool/main/libl/liblzma/liblzma_5.8.1_arm.deb" -LO "https://grimler.se/termux-main/pool/main/g/giflib/giflib_5.2.2_arm.deb" -LO "https://grimler.se/termux-main/pool/main/libt/libtiff/libtiff_4.7.0_arm.deb" -LO "https://grimler.se/termux-main/pool/main/libx/libxml2/libxml2_2.13.7_arm.deb" -LO "https://grimler.se/termux-main/pool/main/z/zstd/zstd_1.5.7_arm.deb"
+
+}
+
+foo(){
+ls *.deb>deb_files
+while IFS= read -r fl;do
+  dpkg -x "$fl" foo
+done <deb_files
+cp -r foo/data/data/com.termux/files/usr/* "$TERMUX_PREFIX"
+#get webp
+curl -L "https://grimler.se/termux-main/pool/main/libw/libwebp/libwebp_1.5.0_arm.deb" -o webp.deb
+dpkg -x webp.deb webp
+cp -r webp/data/data/com.termux/files/usr/* "$TERMUX_PREFIX"
+
+#get jpeg
+curl -L "https://grimler.se/termux-main/pool/main/libj/libjpeg-turbo-static/libjpeg-turbo-static_3.1.0_arm.deb" -o jpeg.deb
+dpkg -x jpeg.deb jpeg
+cp -r jpeg/data/data/com.termux/files/usr/* "$TERMUX_PREFIX"
+
+#get openjpeg
+curl -L "https://grimler.se/termux-main/pool/main/o/openjpeg/openjpeg_2.5.3_arm.deb" -o ojpeg.deb
+dpkg -x ojpeg.deb ojpeg
+cp -r ojpeg/data/data/com.termux/files/usr/* "$TERMUX_PREFIX"
 }
