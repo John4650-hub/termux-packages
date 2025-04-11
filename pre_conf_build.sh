10b524ce49de26580167f5346ed32b908f5e8da3
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-11 11:14:26 +0300

    Created new package preconf

---
+++ pre_conf_build.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+JOHN_PKG_TO_BUILD="$1"
+JOHN_PKG_BUILD_SCRIPT=$(find "./packages/$JOHN_PKG_TO_BUILD" -type f -name "build.sh")
+JOHN_PKG_DEPENDS=$(grep "TERMUX_PKG_DEPENDS" $JOHN_PKG_BUILD_SCRIPT | sed -n 's/.*"\([^"]*\)".*/\1/p')
+
+IFS="," read -r -a dep_array <<< "$JOHN_PKG_DEPENDS"
+
+build_script_paths=("$JOHN_PKG_BUILD_SCRIPT")
+
+for dep_ in "${dep_array[@]}"; do
+  find_exact_path=$(find "./packages/$dep_" -type f -name "build.sh")
+  build_script_paths+="$find_exact_path"
+done
+add_script=$(echo "
+termux_step_pre_configure() {
+        LDFLAGS+=\"-static-libgcc -static-libstdc++ -fopenmp -static-openmp -landroid-posix-semaphore\"
+}
+")
+for p in "${build_script_paths[@]}";do
+  echo "$add_script" >> "$p"
+done
