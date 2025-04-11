43001b7f630a36c8b91cf977288eacf6f6b38fd0
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-11 16:56:49 +0300

    done with  corrections

--- pre_conf_build.sh
+++ pre_conf_build.sh
@@ -9,12 +9,25 @@ build_script_paths=("$JOHN_PKG_BUILD_SCRIPT")
 exclude_these_script=("fontconfig")
 for dep_ in "${dep_array[@]}"; do
   dep_=$(echo "$dep_" | xargs) #remove white space from beginning
-  if [[ "${exclude_these_script[@]}" =~ " $dep_ " ]]; then
+  # Initialize a flag to check if we should skip
+  skip=false
+
+  # Loop through each exclusion
+  for excluded in "${exclude_these_script[@]}"; do
+    if [[ "$excluded" == "$dep_" ]]; then
+      skip=true
+      break
+    fi
+  done
+
+  if $skip; then
     continue
   fi
+
   find_exact_path=$(find "./packages/$dep_" -type f -name "build.sh")
   build_script_paths+=("$find_exact_path")
 done
+
 add_script=$(echo "
 termux_step_pre_configure() {
         LDFLAGS+=\"-static -static-libgcc -static-libstdc++\"
