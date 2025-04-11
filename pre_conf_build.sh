fe57c70b9f61ff7384c7921a8db860cb61b2f53b
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-11 12:43:43 +0300

    done with  corrections

--- pre_conf_build.sh
+++ pre_conf_build.sh
@@ -6,9 +6,12 @@ JOHN_PKG_DEPENDS=$(grep "TERMUX_PKG_DEPENDS" $JOHN_PKG_BUILD_SCRIPT | sed -n 's/
 IFS="," read -r -a dep_array <<< "$JOHN_PKG_DEPENDS"
 
 build_script_paths=("$JOHN_PKG_BUILD_SCRIPT")
-
+exclude_these_script=("fontconfig")
 for dep_ in "${dep_array[@]}"; do
   dep_=$(echo "$dep_" | xargs) #remove white space from beginning
+  if [[ "${exclude_these_script[@]}" =~ " $dep_ "]]; then
+    continue
+  fi
   find_exact_path=$(find "./packages/$dep_" -type f -name "build.sh")
   build_script_paths+=("$find_exact_path")
 done
