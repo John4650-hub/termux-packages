a604dd1213cd933276f60c9aa7ffe58cd11c0a77
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-15 05:13:30 +0300

    commited changes

--- packages/mupdf/build.sh
+++ packages/mupdf/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Lightweight PDF and XPS viewer (library)"
 TERMUX_PKG_LICENSE="AGPL-V3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.24.10"
+TERMUX_PKG_API_LEVEL=23
 TERMUX_PKG_SRCURL=https://mupdf.com/downloads/archive/mupdf-${TERMUX_PKG_VERSION}-source.tar.gz
 TERMUX_PKG_SHA256=939285b5f97caf770fd46cbe7e6cc3a695ab19bb5bfaf5712904549cef390b7b
 TERMUX_PKG_EXTRA_MAKE_ARGS="prefix=$TERMUX_PREFIX build=release libs shared=yes tesseract=no V=1"
