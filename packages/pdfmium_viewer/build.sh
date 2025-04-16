05933ff7d7685b8c1572fe2c1615cd8911acac79
John Delvin <84484398+John4650-hub@users.noreply.github.com>
2025-04-16 13:25:51 +0300

    commited changes

--- packages/pdfmium_viewer/build.sh
+++ packages/pdfmium_viewer/build.sh
@@ -12,6 +12,7 @@ TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_INCLUDE_DIR="$TERMUX_PREFIX/include"
 TERMUX_LIB_DIR="$TERMUX_PREFIX/lib"
+TERMUX_PKG_BUILD_DEPENDS="zlib"
 #download pdfium-no-v8
 curl -L "https://github.com/bblanchon/pdfium-binaries/releases/latest/download/pdfium-android-arm.tgz" -o pdfium-android-arm.tgz
 mkdir foo/ && cd foo
@@ -32,7 +33,7 @@ mkdir argparse
 curl -LO "https://grimler.se/termux-main-21/pool/main/m/mupdf-static/mupdf-static_1.16.1-1_arm.deb"
 dpkg -x mupdf-static_1.16.1-1_arm.deb mupdf_static
 cp -r mupdf_static/data/data/com.termux/files/usr/* "$TERMUX_PREFIX"
-deps=("https://grimler.se/termux-main-21/pool/main/o/openjpeg/openjpeg_2.3.1-2_arm.deb" "https://grimler.se/termux-main-21/pool/main/f/freetype/freetype_2.10.1-2_arm.deb" "https://grimler.se/termux-main-21/pool/main/h/harfbuzz/harfbuzz_2.6.4-1_arm.deb" "https://grimler.se/termux-main-21/pool/main/j/jbig2dec/jbig2dec_0.17-1_arm.deb" "https://grimler.se/termux-main-21/pool/main/g/gumbo-parser/gumbo-parser_0.10.1-1_arm.deb")
+deps=("https://grimler.se/termux-main-21/pool/main/o/openjpeg/openjpeg_2.3.1-2_arm.deb" "https://grimler.se/termux-main-21/pool/main/f/freetype/freetype_2.10.1-2_arm.deb" "https://grimler.se/termux-main-21/pool/main/h/harfbuzz/harfbuzz_2.6.4-1_arm.deb" "https://grimler.se/termux-main-21/pool/main/j/jbig2dec/jbig2dec_0.17-1_arm.deb" "https://grimler.se/termux-main-21/pool/main/g/gumbo-parser/gumbo-parser_0.10.1-1_arm.deb" "https://grimler.se/termux-main-21/pool/main/z/zlib/zlib_1.2.11-2_arm.deb")
 
 mkdir dep
 for url in ${deps[@]}; do
